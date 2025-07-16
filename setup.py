9546eb5d3a52d965725d16408156e5418666b366
Satyam123kumar <kumar.149@iitj.ac.in>
2025-07-16 18:57:33 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI-MediAssist',
+    version= '0.0.0',
+    author= 'Satyam Kumar',
+    author_email= 'satyamkumar14416@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
